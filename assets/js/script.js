a932b42f63ee985e75e48961e87d2b20364fd5bf
Shouvik Dutta <duttashouvik80@gmail.com>
2026-03-02 21:18:13 +0530

    My Portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,91 @@
+
+document.addEventListener("click", function (event) {
+  if (event.target.hasAttribute("data-theme")) {
+    const themeFile = event.target.getAttribute("data-theme");
+    const themeButtons = document.querySelectorAll("[data-theme]");
+    themeButtons.forEach((btn) => btn.classList.remove("active-theme"));
+    event.target.classList.add("active-theme");
+    document.getElementById("themeStylesheet").setAttribute("href", themeFile);
+  }
+});
+
+
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) delta /= 2;
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  // INIT EMAILJS
+  emailjs.init("909AWBwBMRx1glAW_");
+
+  // TYPING INIT
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-type');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+
+  // CONTACT FORM EMAIL SEND
+  const form = document.getElementById("contact-form");
+
+  if (form) {
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      emailjs.sendForm(
+        "service_sfd3zqf",
+        "template_ue1yb85",
+        this
+      ).then(function () {
+        alert("✅ Message sent successfully!");
+        form.reset();
+      }, function (error) {
+        alert("❌ Failed to send message.");
+        console.error(error);
+      });
+    });
+  }
+
+});
